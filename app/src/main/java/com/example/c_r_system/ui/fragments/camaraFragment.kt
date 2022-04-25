eb9a874c0b919bcd42ddbf670a3ba4967e332965
iigulawita <isurugulawita@gmail.com>
2022-04-25 22:11:12 +0530

    second comite(logo update)

---
+++ app/src/main/java/com/example/c_r_system/ui/fragments/camaraFragment.kt
@@ -0,0 +1,29 @@
+package com.example.c_r_system.ui.fragments
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+import com.google.firebase.Timestamp
+
+class camaraFragment : Fragment() {
+
+   // private var _binding: camaraFragmentBinding? = null
+  //  private val binding get() = _binding!!
+
+
+
+
+
+
+
+
+
+
+
+}
